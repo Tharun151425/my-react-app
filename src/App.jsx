353caee5d24e7081e647fb56cb062ce56606ce82
Tharun M <tharun151425@gmail.com>
2025-02-23 23:48:48 +0530

    Added RemoveCard

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,25 @@ import Header from './Header.jsx'
 import Card from './Card.jsx'
 import { useState,useEffect } from 'react'
 function App() {
-  const [cards,setCards] = useState([<Card/>]);
+  const [cards,setCards] = useState([<Card key={0} />]);
+  useEffect(()=>{
+    console.log(cards);
+  },[cards]);
 
+  const removeCard = (Remove_this_key) =>{
+    cards.forEach((card,index) => {
+      if (card.key === Remove_this_key){
+        cards.splice(index,1);
+        return;
+      }
+    })
+  }
   return (<>
     <div>
     <Header setCards={setCards} cards={cards}/>
     </div>
     <div className="card_container">
-    {cards.map((card,index) => (<Card key={index} />))}
+    {cards.map((card,index) => (<Card key={index + 1} />))}
     </div>
     </>
   );
--- src/AppInner.jsx
+++ src/AppInner.jsx
@@ -1,13 +1,15 @@
-function AppInner(props){
-    const sendData = () => {
-        setTimeout(() =>{
-        console.log(`Inside AppInner function`);
-        const currTime = new Date().toLocaleTimeString();
-        props.parentCallBack(currTime);
-        },5000);
-        return (<div></div>)
-    }
-    sendData();
+function useFetch(props){
+    
 }
 
-export default AppInner;
+export default useFetch;
+
+    // const sendData = () => {
+    //     setTimeout(() =>{
+    //     console.log(`Inside AppInner function`);
+    //     const currTime = new Date().toLocaleTimeString();
+    //     props.parentCallBack(currTime);
+    //     },5000);
+    //     return (<div></div>)
+    // }
+    // sendData();
